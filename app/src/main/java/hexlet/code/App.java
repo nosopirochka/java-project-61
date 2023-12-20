b616956a6f26ab74559893126cf1c4c9a576e82c
nosopirochka <dashima.1998@mail.ru>
2023-12-20 13:36:22 +0300

    add App.java

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,38 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\n");
+        System.out.print("Your choice: ");
+        var choice = user.nextInt();
+
+        switch (choice) {
+            case 0:
+                System.out.println("Exit..");
+                break;
+            case 1:
+                Cli.greetings();
+                break;
+            case 2:
+                Engine.getEven();
+                break;
+            case 3:
+                Engine.getCalc();
+                break;
+            case 4:
+                Engine.getGcd();
+                break;
+            case 5:
+                Engine.getProgression();
+                break;
+            case 6:
+                Engine.getPrime();
+                break;
+        }
+    }
+}
