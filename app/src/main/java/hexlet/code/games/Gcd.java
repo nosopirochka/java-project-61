55b4b65d080f40eaf563d92adcbe5fe960557f92
nosopirochka <dashima.1998@mail.ru>
2023-12-20 13:40:19 +0300

    add Gcd.java

---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,27 @@
+package hexlet.code.games;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Gcd {
+    public void getMessage() {
+        System.out.println("Find the greatest common divisor of given numbers.");
+    }
+
+    public String generateExample() {
+        return String.format("%d %d", RandomUtils.nextInt(1, 100), RandomUtils.nextInt(1, 100));
+    }
+
+    public String getAnswer(String answer) {
+        var numbers = answer.split(" ");
+        var number1 = Integer.parseInt(numbers[0]);
+        var number2 = Integer.parseInt(numbers[1]);
+
+        while (number2 != 0) {
+            var number3 = number1 % number2;
+            number1 = number2;
+            number2 = number3;
+        }
+
+        return String.valueOf(number1);
+    }
+}
