75ae5cb392726af1ce5e6cc901ca798d922ec4fd
nosopirochka <dashima.1998@mail.ru>
2023-12-20 13:37:40 +0300

    add Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,142 @@
+package hexlet.code;
+
+import hexlet.code.games.*;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static boolean toControl(String gameAnswer, String answer, String name) {
+        if (gameAnswer.equals(answer)) {
+            System.out.println("Correct!");
+            return true;
+        } else {
+            System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n", answer, gameAnswer);
+            System.out.printf("Let's try again, %s!\n", name);
+            return false;
+        }
+    }
+
+    public static void getCalc() {
+        Scanner myUser = new Scanner(System.in);
+        var name = Cli.greetings();
+        var game = new Calc();
+        game.getMessage();
+
+        for (int i = 0; i < 3; i++) {
+            var question = game.generateExample();
+            System.out.printf("Question: %s\n", question);
+            System.out.print("Your answer: ");
+            var answer = myUser.next();
+            var gameAnswer = game.getAnswer(question);
+
+            var control = Engine.toControl(gameAnswer, answer, name);
+            if (!control) {
+                myUser.close();
+                return;
+            }
+        }
+        myUser.close();
+        System.out.printf("Congratulations, %s!\n", name);
+
+    }
+
+    public static void getEven() {
+        Scanner myUser = new Scanner(System.in);
+        var name = Cli.greetings();
+        var game = new Even();
+        game.getMessage();
+
+        for (int i = 0; i < 3; i++) {
+            var question = game.generateExample();
+            System.out.printf("Question: %s\n", question);
+            System.out.print("Your answer: ");
+            var answer = myUser.next();
+
+            var gameAnswer = game.getAnswer(question);
+
+            var control = Engine.toControl(gameAnswer, answer, name);
+            if (!control) {
+                myUser.close();
+                return;
+            }
+        }
+        myUser.close();
+        System.out.printf("Congratulations, %s!\n", name);
+
+    }
+
+    public static void getGcd() {
+        Scanner myUser = new Scanner(System.in);
+        var name = Cli.greetings();
+        var game = new Gcd();
+        game.getMessage();
+
+        for (int i = 0; i < 3; i++) {
+            var question = game.generateExample();
+            System.out.printf("Question: %s\n", question);
+            System.out.print("Your answer: ");
+            var answer = myUser.next();
+
+            var gameAnswer = game.getAnswer(question);
+
+            var control = Engine.toControl(gameAnswer, answer, name);
+            if (!control) {
+                myUser.close();
+                return;
+            }
+        }
+        myUser.close();
+        System.out.printf("Congratulations, %s!\n", name);
+
+    }
+
+    public static void getPrime() {
+        Scanner myUser = new Scanner(System.in);
+        var name = Cli.greetings();
+        var game = new Prime();
+        game.getMessage();
+
+        for (int i = 0; i < 3; i++) {
+            var question = game.generateExample();
+            System.out.printf("Question: %s\n", question);
+            System.out.print("Your answer: ");
+            var answer = myUser.next();
+
+            var gameAnswer = game.getAnswer(question);
+
+            var control = Engine.toControl(gameAnswer, answer, name);
+            if (!control) {
+                myUser.close();
+                return;
+            }
+        }
+        myUser.close();
+        System.out.printf("Congratulations, %s!\n", name);
+
+    }
+
+    public static void getProgression() {
+        Scanner myUser = new Scanner(System.in);
+        var name = Cli.greetings();
+        var game = new Progression();
+        game.getMessage();
+
+        for (int i = 0; i < 3; i++) {
+            var question = game.generateExample();
+            System.out.printf("Question: %s\n", question);
+            System.out.print("Your answer: ");
+            var answer = myUser.next();
+
+            var gameAnswer = game.getAnswer(question);
+
+            var control = Engine.toControl(gameAnswer, answer, name);
+            if (!control) {
+                myUser.close();
+                return;
+            }
+        }
+        myUser.close();
+        System.out.printf("Congratulations, %s!\n", name);
+
+    }
+}
