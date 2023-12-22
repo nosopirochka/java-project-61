36f3512058f29f956d1afeb9a5763df9a9f52741
nosopirochka <dashima.1998@mail.ru>
2023-12-22 15:51:34 +0300

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,16 +1,30 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import org.apache.commons.lang3.RandomUtils;
 
 public class Prime {
-    public String generateExample() {
+    public static void playPrime() {
+        var messageForGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[] example = new String[3];
+        String[] answers = new String[3];
+        var forTry = 3;
+        for (var i = 0; i < forTry; i++) {
+            var exampleFromGenerate = generateExample();
+            example[i] = exampleFromGenerate;
+            var answerFromGenerate = getAnswer(exampleFromGenerate);
+            answers[i] = answerFromGenerate;
+        }
+        Engine.playGame(messageForGame, example, answers);
+    }
+    public static String generateExample() {
         var minValueForNumber = 2;
         var maxValueForNumber = 30;
 
         return String.valueOf(RandomUtils.nextInt(minValueForNumber, maxValueForNumber));
     }
 
-    public String getAnswer(String answer) {
+    public static String getAnswer(String answer) {
         var number = Integer.parseInt(answer);
         var stop = number / 2;
         for (int i = 2; i <= stop; i++) {
