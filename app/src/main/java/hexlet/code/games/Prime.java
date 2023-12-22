dbf19df0caf13a7b7fc717251b78bb44ec1ea530
nosopirochka <dashima.1998@mail.ru>
2023-12-22 17:21:41 +0300

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,12 +4,14 @@ import hexlet.code.Engine;
 import org.apache.commons.lang3.RandomUtils;
 
 public class Prime {
+    static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
+    static final int MIN_VALUE_FOR_NUMBER = 2;
+    static final int MAX_VALUE_FOR_NUMBER = 30;
     public static void playPrime() {
         var messageForGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        var arraySize = 3;
-        String[] example = new String[arraySize];
-        String[] answers = new String[arraySize];
-        for (var i = 0; i < arraySize; i++) {
+        String[] example = new String[AMOUNT_OF_EXAMPLES_AND_ANSWERS];
+        String[] answers = new String[AMOUNT_OF_EXAMPLES_AND_ANSWERS];
+        for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
             var exampleFromGenerate = generateExample();
             example[i] = exampleFromGenerate;
             var answerFromGenerate = getAnswer(exampleFromGenerate);
@@ -18,10 +20,7 @@ public class Prime {
         Engine.playGame(messageForGame, example, answers);
     }
     public static String generateExample() {
-        var minValueForNumber = 2;
-        var maxValueForNumber = 30;
-
-        return String.valueOf(RandomUtils.nextInt(minValueForNumber, maxValueForNumber));
+        return String.valueOf(RandomUtils.nextInt(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
     }
 
     public static String getAnswer(String answer) {
