f72e86af1af9c1e773c8fd00be38d9df8a12246e
nosopirochka <dashima.1998@mail.ru>
2023-12-22 15:50:09 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,9 +1,26 @@
 package hexlet.code.games;
 
 import org.apache.commons.lang3.RandomUtils;
+import hexlet.code.Engine;
 
 public class Calc {
-    public String generateExample() {
+
+    public static void playCalc() {
+        var messageForGame = "What is the result of the expression?";
+        String[] example = new String[3];
+        String[] answers = new String[3];
+        var forTry = 3;
+        for (var i = 0; i < forTry; i++) {
+            var exampleFromGenerate = generateExample();
+            example[i] = exampleFromGenerate;
+            var answerFromGenerate = getAnswer(exampleFromGenerate);
+            answers[i] = answerFromGenerate;
+        }
+        Engine.playGame(messageForGame, example, answers);
+
+
+    }
+    public static String generateExample() {
         var minValueForNumber = 1;
         var maxValueForNumber = 10;
         var number1 = RandomUtils.nextInt(minValueForNumber, maxValueForNumber);
@@ -12,8 +29,8 @@ public class Calc {
         return String.format("%s %s %s", String.valueOf(number1), Calc.generateSign(), String.valueOf(number2));
     }
 
-    public String getAnswer(String answer) {
-        var info = answer.split(" ");
+    public static String getAnswer(String example) {
+        var info = example.split(" ");
         if (info[1].equals("+")) {
             return String.valueOf(Integer.parseInt(info[0]) + Integer.parseInt(info[2]));
         } else if (info[1].equals("*")) {
@@ -23,7 +40,7 @@ public class Calc {
         }
     }
 
-    static String generateSign() {
+    public static String generateSign() {
         String[] signs = {"-", "+", "*"};
         return signs[RandomUtils.nextInt(0, signs.length - 1)];
     }
