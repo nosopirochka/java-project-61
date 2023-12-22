b972261b5b43c387495262eb734e12cc46ace923
nosopirochka <dashima.1998@mail.ru>
2023-12-22 15:49:38 +0300

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,161 +1,26 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.Gcd;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-
 import java.util.Scanner;
 
 public class Engine {
-    public static boolean toControl(String gameAnswer, String answer, String name) {
-        if (gameAnswer.equals(answer)) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n", answer, gameAnswer);
-            System.out.printf("Let's try again, %s!\n", name);
-            return false;
-        }
-    }
-
-    public static void getMeetings() {
+    public static void playGame(String message, String[] gameExamples, String[] gameAnswers) {
+        var sc = new Scanner(System.in);
         var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-    }
-
-    public static void getCalc() {
-        Scanner myUser = new Scanner(System.in);
-        var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-        var game = new Calc();
-        System.out.println("What is the result of the expression?");
-        var attempt = 3;
-
-        for (int i = 0; i < attempt; i++) {
-            var question = game.generateExample();
-            System.out.printf("Question: %s\n", question);
+        System.out.println(message);
+        var forTry = 3;
+        for (int i = 0; i < forTry; i++) {
+            System.out.println("Question: " + gameExamples[i]);
             System.out.print("Your answer: ");
-            var answer = myUser.next();
-            var gameAnswer = game.getAnswer(question);
-
-            var control = Engine.toControl(gameAnswer, answer, name);
-            if (!control) {
-                myUser.close();
+            var answer = sc.next();
+            if (gameAnswers[i].equals(answer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n", answer, gameAnswers[i]);
+                System.out.printf("Let's try again, %s!\n", name);
                 return;
             }
         }
-        myUser.close();
-        System.out.printf("Congratulations, %s!\n", name);
-
-    }
-
-    public static void getEven() {
-        Scanner myUser = new Scanner(System.in);
-        var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-        var game = new Even();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        var attempt = 3;
-
-        for (int i = 0; i < attempt; i++) {
-            var question = game.generateExample();
-            System.out.printf("Question: %s\n", question);
-            System.out.print("Your answer: ");
-            var answer = myUser.next();
-
-            var gameAnswer = game.getAnswer(question);
-
-            var control = Engine.toControl(gameAnswer, answer, name);
-            if (!control) {
-                myUser.close();
-                return;
-            }
-        }
-        myUser.close();
-        System.out.printf("Congratulations, %s!\n", name);
-
-    }
-
-    public static void getGcd() {
-        Scanner myUser = new Scanner(System.in);
-        var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-        var game = new Gcd();
-        System.out.println("Find the greatest common divisor of given numbers.");
-        var attempt = 3;
-
-        for (int i = 0; i < attempt; i++) {
-            var question = game.generateExample();
-            System.out.printf("Question: %s\n", question);
-            System.out.print("Your answer: ");
-            var answer = myUser.next();
-
-            var gameAnswer = game.getAnswer(question);
-
-            var control = Engine.toControl(gameAnswer, answer, name);
-            if (!control) {
-                myUser.close();
-                return;
-            }
-        }
-        myUser.close();
-        System.out.printf("Congratulations, %s!\n", name);
-
-    }
-
-    public static void getPrime() {
-        Scanner myUser = new Scanner(System.in);
-        var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-        var game = new Prime();
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        var attempt = 3;
-
-        for (int i = 0; i < attempt; i++) {
-            var question = game.generateExample();
-            System.out.printf("Question: %s\n", question);
-            System.out.print("Your answer: ");
-            var answer = myUser.next();
-
-            var gameAnswer = game.getAnswer(question);
-
-            var control = Engine.toControl(gameAnswer, answer, name);
-            if (!control) {
-                myUser.close();
-                return;
-            }
-        }
-        myUser.close();
-        System.out.printf("Congratulations, %s!\n", name);
-
-    }
-
-    public static void getProgression() {
-        Scanner myUser = new Scanner(System.in);
-        var name = Cli.greetings();
-        System.out.printf("Hello, %s!\n", name);
-        var game = new Progression();
-        System.out.println("What number is missing in the progression?");
-        var attempt = 3;
-
-        for (int i = 0; i < attempt; i++) {
-            var question = game.generateExample();
-            System.out.printf("Question: %s\n", question);
-            System.out.print("Your answer: ");
-            var answer = myUser.next();
-
-            var gameAnswer = game.getAnswer(question);
-
-            var control = Engine.toControl(gameAnswer, answer, name);
-            if (!control) {
-                myUser.close();
-                return;
-            }
-        }
-        myUser.close();
-        System.out.printf("Congratulations, %s!\n", name);
-
+        sc.close();
+        System.out.println("Congratulations, " + name + "!");
     }
 }
