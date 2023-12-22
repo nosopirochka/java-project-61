9f30fbeda3f6ec46e9692a837a7a11532fa3fb85
nosopirochka <dashima.1998@mail.ru>
2023-12-22 15:50:33 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,9 +1,24 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import org.apache.commons.lang3.RandomUtils;
 
 public class Even {
-    public String generateExample() {
+    public static void playEven() {
+        var messageForGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        String[] example = new String[3];
+        String[] answers = new String[3];
+        var forTry = 3;
+        for (var i = 0; i < forTry; i++) {
+            var exampleFromGenerate = generateExample();
+            example[i] = exampleFromGenerate;
+            var answerFromGenerate = getAnswer(exampleFromGenerate);
+            answers[i] = answerFromGenerate;
+        }
+        Engine.playGame(messageForGame, example, answers);
+    }
+
+    public static String generateExample() {
         var minValueForNumber = 1;
         var maxValueForNumber = 100;
         int randomNumber = RandomUtils.nextInt(minValueForNumber, maxValueForNumber);
@@ -11,7 +26,7 @@ public class Even {
         return String.valueOf(randomNumber);
     }
 
-    public String getAnswer(String answer) {
+    public static String getAnswer(String answer) {
         var compareNumber = Integer.parseInt(answer);
         if (compareNumber % 2 == 0) {
             return "yes";
