34f67804a5721e40772cf4b053267e06f6a797d5
nosopirochka <dashima.1998@mail.ru>
2023-12-25 18:41:00 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,20 +9,20 @@ import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
 public class App {
-    static final int EXIT = 0;
-    static final int GREET = 1;
-    static final int EVEN = 2;
-    static final int CALC = 3;
-    static final int GCD = 4;
-    static final int PROGRESSION = 5;
-    static final int PRIME = 6;
+    static final String EXIT = "0";
+    static final String GREET = "1";
+    static final String EVEN = "2";
+    static final String CALC = "3";
+    static final String GCD = "4";
+    static final String PROGRESSION = "5";
+    static final String PRIME = "6";
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
 
         System.out.println("Please enter the game number and press Enter.");
         System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\n");
         System.out.print("Your choice: ");
-        int choice = user.nextInt();
+        var choice = user.next();
 
         switch (choice) {
             case EXIT:
