36cec3ddf7a1ed6b2ea455cdc54223a57118e957
nosopirochka <dashima.1998@mail.ru>
2023-12-26 14:18:18 +0300

    add Utils.java

---
+++ app/src/main/java/hexlet/code/games/Utils.java
@@ -0,0 +1,18 @@
+package hexlet.code.games;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Utils {
+    public static int getOneNumber(int minNumber, int maxNumber) {
+        return RandomUtils.nextInt(minNumber, maxNumber);
+    }
+    public static int[] getTwoNumbers(int minNumber, int maxNumber) {
+        int[] arrayOfNumbers = new int[2];
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            var randomNumber = RandomUtils.nextInt(minNumber, maxNumber);
+            arrayOfNumbers[i] = randomNumber;
+        }
+
+        return arrayOfNumbers;
+    }
+}
