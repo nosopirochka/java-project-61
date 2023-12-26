859943e773fa38cecbbea94266bb712bc60637dd
nosopirochka <dashima.1998@mail.ru>
2023-12-26 14:27:38 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,13 +1,12 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import org.apache.commons.lang3.RandomUtils;
+
 
 public class Even {
     static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 100;
-
     public static void run() {
         var messageForGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         var examplesAndAnswers = generateData();
