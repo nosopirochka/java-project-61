b304005b3fd904fdd5eec21c3952ea5746481b19
nosopirochka <dashima.1998@mail.ru>
2023-12-26 14:15:41 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -7,25 +7,31 @@ public class Calc {
     static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 10;
-    public static void playCalc() {
+    public static void run() {
         var messageForGame = "What is the result of the expression?";
-        String[] example = new String[AMOUNT_OF_EXAMPLES_AND_ANSWERS];
-        String[] answers = new String[AMOUNT_OF_EXAMPLES_AND_ANSWERS];
+        var examplesAndAnswers = generateData();
+        Engine.playGame(messageForGame, examplesAndAnswers[0], examplesAndAnswers[1]);
+    }
+
+    public static String[][] generateData() {
+        var lengthOfArray = 2;
+        String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
         for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
             var exampleFromGenerate = generateExample();
-            example[i] = exampleFromGenerate;
+            examplesAndAnswers[0][i] = exampleFromGenerate;
             var answerFromGenerate = getAnswer(exampleFromGenerate);
-            answers[i] = answerFromGenerate;
+            examplesAndAnswers[1][i] = answerFromGenerate;
         }
-        Engine.playGame(messageForGame, example, answers);
 
-
+        return examplesAndAnswers;
     }
-    public static String generateExample() {
-        var number1 = RandomUtils.nextInt(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
-        var number2 = RandomUtils.nextInt(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
 
-        return String.format("%s %s %s", String.valueOf(number1), Calc.generateSign(), String.valueOf(number2));
+    public static String generateExample() {
+        var numbersForExample = Utils.getTwoNumbers(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+        String[] signs = {"-", "+", "*"};
+        var randomSign = signs[Utils.getOneNumber(0, signs.length - 1)];
+
+        return String.format("%d %s %d", numbersForExample[0], randomSign, numbersForExample[1]);
     }
 
     public static String getAnswer(String example) {
@@ -37,10 +43,5 @@ public class Calc {
         } else {
             return String.valueOf(Integer.parseInt(info[0]) - Integer.parseInt(info[2]));
         }
-    }
-
-    public static String generateSign() {
-        String[] signs = {"-", "+", "*"};
-        return signs[RandomUtils.nextInt(0, signs.length - 1)];
     }
 }
