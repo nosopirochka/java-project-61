925f9e54d4766360ed01a55bacd3a0b43bdea0aa
nosopirochka <dashima.1998@mail.ru>
2024-01-04 13:33:52 +0300

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -7,13 +7,4 @@ public class Utils {
     public static int getOneNumber(int minNumber, int maxNumber) {
         return RandomUtils.nextInt(minNumber, maxNumber);
     }
-    public static int[] getTwoNumbers(int minNumber, int maxNumber) {
-        int[] arrayOfNumbers = new int[2];
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
-            var randomNumber = RandomUtils.nextInt(minNumber, maxNumber);
-            arrayOfNumbers[i] = randomNumber;
-        }
-
-        return arrayOfNumbers;
-    }
 }
