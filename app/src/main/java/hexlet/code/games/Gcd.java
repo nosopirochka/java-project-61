ed78cc5df98ac6ffb1dc54eb96f144fef85e04f9
nosopirochka <dashima.1998@mail.ru>
2024-01-25 20:01:08 +0300

    update Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -18,37 +18,20 @@ public class Gcd {
         var lengthOfArray = 2;
         String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
         for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
-            var exampleFromGenerate = getExample();
-            examplesAndAnswers[0][i] = exampleFromGenerate;
-            var answerFromGenerate = getAnswer(exampleFromGenerate);
-            examplesAndAnswers[1][i] = answerFromGenerate;
+            var numberForExample1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+            var numberForExample2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+            //запись примера в array
+            examplesAndAnswers[0][i] = String.format("%d %d", numberForExample1, numberForExample2);
+
+            while (numberForExample2  != 0) {
+                var number3 = numberForExample1 % numberForExample2;
+                numberForExample1 = numberForExample2 ;
+                numberForExample2  = number3;
+            }
+            //запись ответа в array
+            examplesAndAnswers[1][i] = String.valueOf(numberForExample1);
         }
 
         return examplesAndAnswers;
-    }
-
-    private static int makeNumberForGcd() {
-        return Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
-    }
-
-    public static String getExample() {
-        var numberForExample1 = makeNumberForGcd();
-        var numberForExample2 = makeNumberForGcd();
-
-        return String.format("%d %d", numberForExample1, numberForExample2);
-    }
-
-    public static String getAnswer(String answer) {
-        var numbers = answer.split(" ");
-        var number1 = Integer.parseInt(numbers[0]);
-        var number2 = Integer.parseInt(numbers[1]);
-
-        while (number2 != 0) {
-            var number3 = number1 % number2;
-            number1 = number2;
-            number2 = number3;
-        }
-
-        return String.valueOf(number1);
     }
 }
