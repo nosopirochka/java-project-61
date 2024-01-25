014680f1a2c009912b7a9bde1eaf64d37c1b822e
nosopirochka <dashima.1998@mail.ru>
2024-01-25 20:01:29 +0300

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -18,38 +18,27 @@ public class Prime {
         var lengthOfArray = 2;
         String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
         for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
-            var exampleFromGenerate = getExample();
+            var exampleFromGenerate = String.valueOf(Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
+            //запись примера в array
             examplesAndAnswers[0][i] = exampleFromGenerate;
-            var answerFromGenerate = getAnswer(exampleFromGenerate);
+            var number = Integer.parseInt(exampleFromGenerate);
+            var answerFromGenerate = makeAnswer(number) ? "yes" : "no";
+            //запись ответа в array
             examplesAndAnswers[1][i] = answerFromGenerate;
         }
 
         return examplesAndAnswers;
     }
 
-    private static int makeNumber() {
-        return Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
-    }
-    public static String getExample() {
-        return String.valueOf(makeNumber());
-    }
-
-    public static String getAnswer(String answer) {
-        var number = Integer.parseInt(answer);
+    public static boolean makeAnswer(int number) {
         var stop = number / 2;
-
-        return makeAnswer(number, stop);
-    }
-
-    public static String makeAnswer(int number, int stop) {
         for (int i = 2; i <= stop; i++) {
-            var truOrNot = number % i == 0;
-            var result = truOrNot ? "no" : "yes";
-            if (result.equals("no")) {
-                return "no";
+            var result = number % i == 0;
+            if (result) {
+                return false;
             }
         }
 
-        return "yes";
+        return true;
     }
 }
