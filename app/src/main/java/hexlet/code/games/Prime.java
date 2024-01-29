7e6330319c2985cc9f58b2e684692f4e9785cd4c
nosopirochka <dashima.1998@mail.ru>
2024-01-29 21:24:51 +0300

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,30 +5,34 @@ import hexlet.code.Utils;
 
 
 public class Prime {
-    static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 2;
     static final int MAX_VALUE_FOR_NUMBER = 30;
+    static final int INDEX_OF_EXAMPLES = 0;
+    static final int INDEX_OF_ANSWERS = 1;
+
+
     public static void run() {
         var messageForGame = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[0], examplesAndAnswers[1]);
+        Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
     }
+
 
     public static String[][] getData() {
         var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
-        for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
+        String[][] examplesAndAnswers = new String[lengthOfArray][Engine.COUNT_OF_ROUNDS];
+        for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             var exampleFromGenerate = String.valueOf(Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
-            //запись примера в array
-            examplesAndAnswers[0][i] = exampleFromGenerate;
             var number = Integer.parseInt(exampleFromGenerate);
             var answerFromGenerate = makeAnswer(number) ? "yes" : "no";
+            //запись примера в array
+            examplesAndAnswers[0][i] = exampleFromGenerate;
             //запись ответа в array
             examplesAndAnswers[1][i] = answerFromGenerate;
         }
-
         return examplesAndAnswers;
     }
+
 
     public static boolean makeAnswer(int number) {
         var stop = number / 2;
@@ -38,7 +42,6 @@ public class Prime {
                 return false;
             }
         }
-
         return true;
     }
 }
