05cb0a77fad8f40726465b5cd17a3399d13eb118
nosopirochka <dashima.1998@mail.ru>
2024-01-29 21:23:45 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,36 +5,48 @@ import hexlet.code.Utils;
 
 
 public class Calc {
-    static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 10;
+    static final int INDEX_OF_EXAMPLES = 0;
+    static final int INDEX_OF_ANSWERS = 1;
+    static final int LENGTH_OF_ARRAY_WITH_EXAMPLES_AND_ANSWERS = 2;
+
+
     public static void run() {
         var messageForGame = "What is the result of the expression?";
-        var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[0], examplesAndAnswers[1]);
+        try {
+            var examplesAndAnswers = getData();
+            Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static String[][] getData() {
-        var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
-        for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
-            String[] signs = {"-", "+", "*"};
+
+    public static String[][] getData() throws Exception {
+        String[][] examplesAndAnswers = new String[LENGTH_OF_ARRAY_WITH_EXAMPLES_AND_ANSWERS][Engine.COUNT_OF_ROUNDS];
+        for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
+            String[] signs = {"-", "+", "*", "\\", "\\", "\\"};
             var number1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var number2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var randomSign = signs[Utils.getOneNumber(0, signs.length - 1)];
             var example = String.format("%d %s %d", number1, randomSign, number2);
+            var answer = String.valueOf(makeAnswer(number1, number2, randomSign));
             //запись примера в array
             examplesAndAnswers[0][i] = example;
-            var info = example.split(" ");
-            var answer = switch (info[1]) {
-                case "+" -> String.valueOf(Integer.parseInt(info[0]) + Integer.parseInt(info[2]));
-                case "*" -> String.valueOf(Integer.parseInt(info[0]) * Integer.parseInt(info[2]));
-                default -> String.valueOf(Integer.parseInt(info[0]) - Integer.parseInt(info[2]));
-            };
             //запись ответа в array
             examplesAndAnswers[1][i] = answer;
         }
-
         return examplesAndAnswers;
+    }
+
+
+    public static int makeAnswer(int number1, int number2, String sign) throws Exception {
+        return switch (sign) {
+            case "+" -> number1 + number2;
+            case "*" -> number1 * number2;
+            case "-" -> number1 - number2;
+            default -> throw new Exception(String.format("Error: create operation for operator \"%s\"", sign));
+        };
     }
 }
