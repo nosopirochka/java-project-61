63a9dd9f75ea11929153a20033fd1dca4b89923b
nosopirochka <dashima.1998@mail.ru>
2024-01-29 21:22:57 +0300

    update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.RandomUtils;
 
 
 public class Utils {
+
+
     public static int getOneNumber(int minNumber, int maxNumber) {
         return RandomUtils.nextInt(minNumber, maxNumber);
     }
