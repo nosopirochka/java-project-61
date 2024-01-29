ee0e00f795fd09c194f3a6fdbf328023237c8a71
nosopirochka <dashima.1998@mail.ru>
2024-01-29 21:24:05 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,31 +5,34 @@ import hexlet.code.Utils;
 
 
 public class Even {
-    static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 100;
+    static final int INDEX_OF_EXAMPLES = 0;
+    static final int INDEX_OF_ANSWERS = 1;
+
+
     public static void run() {
         var messageForGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[0], examplesAndAnswers[1]);
+        Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
     }
+
 
     public static String[][] getData() {
         var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
-        for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
-            var exampleFromGenerate = String.valueOf(Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
+        String[][] examplesAndAnswers = new String[lengthOfArray][Engine.COUNT_OF_ROUNDS];
+        for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
+            var example = String.valueOf(Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
+            boolean even = makeAnswer(example);
+            var answer = even ? "yes" : "no";
             //запись примера в array
-            examplesAndAnswers[0][i] = exampleFromGenerate;
-            String answerFromGenerate;
-            boolean even = makeAnswer(exampleFromGenerate);
-            answerFromGenerate = even ? "yes" : "no";
+            examplesAndAnswers[0][i] = example;
             //запись ответа в array
-            examplesAndAnswers[1][i] = answerFromGenerate;
+            examplesAndAnswers[1][i] = answer;
         }
-
         return examplesAndAnswers;
     }
+
 
     public static boolean makeAnswer(String answer) {
         var compareNumber = Integer.parseInt(answer);
