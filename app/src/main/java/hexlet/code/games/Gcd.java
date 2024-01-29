1beae3aa4eb50b2b942b0a338bb2c57c88c767ee
nosopirochka <dashima.1998@mail.ru>
2024-01-29 21:24:24 +0300

    update Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -5,33 +5,42 @@ import hexlet.code.Utils;
 
 
 public class Gcd {
-    static final int AMOUNT_OF_EXAMPLES_AND_ANSWERS = 3;
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 100;
+    static final int INDEX_OF_EXAMPLES = 0;
+    static final int INDEX_OF_ANSWERS = 1;
+
+
     public static void run() {
         var messageForGame = "Find the greatest common divisor of given numbers.";
         var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[0], examplesAndAnswers[1]);
+        Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
     }
+
 
     public static String[][] getData() {
         var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][AMOUNT_OF_EXAMPLES_AND_ANSWERS];
-        for (var i = 0; i < AMOUNT_OF_EXAMPLES_AND_ANSWERS; i++) {
+        String[][] examplesAndAnswers = new String[lengthOfArray][Engine.COUNT_OF_ROUNDS];
+        for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             var numberForExample1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var numberForExample2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+            var example = String.format("%d %d", numberForExample1, numberForExample2);
+            var answer = String.valueOf(makeAnswer(numberForExample1, numberForExample2));
             //запись примера в array
-            examplesAndAnswers[0][i] = String.format("%d %d", numberForExample1, numberForExample2);
-
-            while (numberForExample2  != 0) {
-                var number3 = numberForExample1 % numberForExample2;
-                numberForExample1 = numberForExample2;
-                numberForExample2  = number3;
-            }
+            examplesAndAnswers[0][i] = example;
             //запись ответа в array
-            examplesAndAnswers[1][i] = String.valueOf(numberForExample1);
+            examplesAndAnswers[1][i] = answer;
         }
-
         return examplesAndAnswers;
+    }
+
+
+    public static int makeAnswer(int number1, int number2) {
+        while (number2  != 0) {
+            var number3 = number1 % number2;
+            number1 = number2;
+            number2  = number3;
+        }
+        return number1;
     }
 }
