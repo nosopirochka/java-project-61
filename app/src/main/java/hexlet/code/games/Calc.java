2e4a1ae4ad8a298eac474bfc57b987a581282de1
nosopirochka <dashima.1998@mail.ru>
2024-02-02 14:10:45 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -2,48 +2,43 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class Calc {
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 10;
-    static final int INDEX_OF_EXAMPLES = 0;
-    static final int INDEX_OF_ANSWERS = 1;
-    static final int LENGTH_OF_ARRAY_WITH_EXAMPLES_AND_ANSWERS = 2;
 
 
     public static void run() {
         var messageForGame = "What is the result of the expression?";
         try {
-            var examplesAndAnswers = getData();
-            Engine.playGame(messageForGame,
-                    examplesAndAnswers[INDEX_OF_EXAMPLES],
-                    examplesAndAnswers[INDEX_OF_ANSWERS]);
+            var examplesAndAnswers = generateDataForCalc();
+            Engine.playGame(messageForGame, examplesAndAnswers);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
 
-    public static String[][] getData() throws Exception {
-        String[][] examplesAndAnswers = new String[LENGTH_OF_ARRAY_WITH_EXAMPLES_AND_ANSWERS][Engine.COUNT_OF_ROUNDS];
+    public static Map<String, String> generateDataForCalc() throws Exception {
+        Map<String, String> examplesAndAnswers = new HashMap<>();
         for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             String[] signs = {"-", "+", "*"};
-            var number1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
-            var number2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+            var numberForExample1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
+            var numberForExample2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var randomSign = signs[Utils.getOneNumber(0, signs.length - 1)];
-            var example = String.format("%d %s %d", number1, randomSign, number2);
-            var answer = String.valueOf(makeAnswer(number1, number2, randomSign));
-            //запись примера в array
-            examplesAndAnswers[0][i] = example;
-            //запись ответа в array
-            examplesAndAnswers[1][i] = answer;
+            var example = String.format("%d %s %d", numberForExample1, randomSign, numberForExample2);
+            var answer = String.valueOf(calculate(numberForExample1, numberForExample2, randomSign));
+            //recording the answer in Map
+            examplesAndAnswers.put(example, answer);
         }
         return examplesAndAnswers;
     }
 
 
-    public static int makeAnswer(int number1, int number2, String sign) throws Exception {
+    public static int calculate(int number1, int number2, String sign) throws Exception {
         return switch (sign) {
             case "+" -> number1 + number2;
             case "*" -> number1 * number2;
