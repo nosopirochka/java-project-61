12bcc58c8e3dc4d91a4e97a13baf6890abd0174e
nosopirochka <dashima.1998@mail.ru>
2024-02-02 14:09:36 +0300

    update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,33 +1,36 @@
 package hexlet.code;
 
 import java.util.Scanner;
+import java.util.Map;
 
 public class Engine {
     public static final int COUNT_OF_ROUNDS = 3;
 
 
-    public static void playGame(String message, String[] gameExamples, String[] gameAnswers) {
-        var sc = new Scanner(System.in);
+    public static void playGame(String message, Map<String, String> examplesAndAnswers) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        var name = sc.next();
+        var name = scanner.next();
         System.out.println("Hello, " + name + "!");
         System.out.println(message);
 
-        for (int i = 0; i < gameExamples.length; i++) {
-            System.out.println("Question: " + gameExamples[i]);
+        for (var example: examplesAndAnswers.keySet()) {
+            System.out.println("Question: " + example);
             System.out.print("Your answer: ");
-            var answer = sc.next();
-            if (gameAnswers[i].equals(answer)) {
+            var answer = scanner.next();
+            if (examplesAndAnswers.get(example).equals(answer)) {
                 System.out.println("Correct!");
             } else {
-                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n", answer, gameAnswers[i]);
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n",
+                        answer,
+                        examplesAndAnswers.get(example));
                 System.out.printf("Let's try again, %s!\n", name);
-                sc.close();
+                scanner.close();
                 return;
             }
         }
-        sc.close();
+        scanner.close();
         System.out.println("Congratulations, " + name + "!");
     }
 }
