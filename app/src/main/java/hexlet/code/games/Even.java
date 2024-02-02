0705b70d6065b287855bceae500744bf391dea25
nosopirochka <dashima.1998@mail.ru>
2024-02-02 14:11:15 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -2,39 +2,37 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
+import java.util.Map;
+import java.util.HashMap;
+
 
 
 public class Even {
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 100;
-    static final int INDEX_OF_EXAMPLES = 0;
-    static final int INDEX_OF_ANSWERS = 1;
 
 
     public static void run() {
         var messageForGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
+        var examplesAndAnswers = generateDataForEven();
+        Engine.playGame(messageForGame, examplesAndAnswers);
     }
 
 
-    public static String[][] getData() {
-        var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][Engine.COUNT_OF_ROUNDS];
+    public static Map<String, String> generateDataForEven() {
+        Map<String, String> examplesAndAnswers = new HashMap<>();
         for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             var example = String.valueOf(Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER));
-            boolean even = makeAnswer(example);
-            var answer = even ? "yes" : "no";
-            //запись примера в array
-            examplesAndAnswers[0][i] = example;
-            //запись ответа в array
-            examplesAndAnswers[1][i] = answer;
+            boolean evenOrNot = getAnswerEvenOrNot(example);
+            var answer = evenOrNot ? "yes" : "no";
+            //recording the answer in Map
+            examplesAndAnswers.put(example, answer);
         }
         return examplesAndAnswers;
     }
 
 
-    public static boolean makeAnswer(String answer) {
+    public static boolean getAnswerEvenOrNot(String answer) {
         var compareNumber = Integer.parseInt(answer);
         return compareNumber % 2 == 0;
     }
