eb8eb8412381bfd006c546a6fbf791e3186cf69b
nosopirochka <dashima.1998@mail.ru>
2024-02-02 14:11:36 +0300

    update Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -2,40 +2,37 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class Gcd {
     static final int MIN_VALUE_FOR_NUMBER = 1;
     static final int MAX_VALUE_FOR_NUMBER = 100;
-    static final int INDEX_OF_EXAMPLES = 0;
-    static final int INDEX_OF_ANSWERS = 1;
 
 
     public static void run() {
         var messageForGame = "Find the greatest common divisor of given numbers.";
-        var examplesAndAnswers = getData();
-        Engine.playGame(messageForGame, examplesAndAnswers[INDEX_OF_EXAMPLES], examplesAndAnswers[INDEX_OF_ANSWERS]);
+        var examplesAndAnswers = generateDataForGcd();
+        Engine.playGame(messageForGame, examplesAndAnswers);
     }
 
 
-    public static String[][] getData() {
-        var lengthOfArray = 2;
-        String[][] examplesAndAnswers = new String[lengthOfArray][Engine.COUNT_OF_ROUNDS];
+    public static Map<String, String> generateDataForGcd() {
+        Map<String, String> examplesAndAnswers = new HashMap<>();
         for (var i = 0; i < Engine.COUNT_OF_ROUNDS; i++) {
             var numberForExample1 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var numberForExample2 = Utils.getOneNumber(MIN_VALUE_FOR_NUMBER, MAX_VALUE_FOR_NUMBER);
             var example = String.format("%d %d", numberForExample1, numberForExample2);
-            var answer = String.valueOf(makeAnswer(numberForExample1, numberForExample2));
-            //запись примера в array
-            examplesAndAnswers[0][i] = example;
-            //запись ответа в array
-            examplesAndAnswers[1][i] = answer;
+            var answer = String.valueOf(getGreatestCommonDivisor(numberForExample1, numberForExample2));
+            //recording the answer in Map
+            examplesAndAnswers.put(example, answer);
         }
         return examplesAndAnswers;
     }
 
 
-    public static int makeAnswer(int number1, int number2) {
+    public static int getGreatestCommonDivisor(int number1, int number2) {
         while (number2  != 0) {
             var number3 = number1 % number2;
             number1 = number2;
