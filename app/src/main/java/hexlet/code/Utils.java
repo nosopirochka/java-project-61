76732339e164fe9e9b1653ede107e3562bcb4723
nosopirochka <dashima.1998@mail.ru>
2024-02-06 19:21:06 +0300

    style: delete spaces

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import org.apache.commons.lang3.RandomUtils;
 
-
 public class Utils {
     public static int getOneNumber(int minNumber, int maxNumber) {
         return RandomUtils.nextInt(minNumber, maxNumber);
